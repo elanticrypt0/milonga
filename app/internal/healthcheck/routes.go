5492ab7157ca9a5adad904e9fffca961c7d8180d
k23dev <hashersmasher@proton.me>
2024-12-31 08:17:35 -0300

    add ping function

--- app/internal/healthcheck/routes.go
+++ app/internal/healthcheck/routes.go
@@ -3,12 +3,20 @@ package healthcheck
 import (
 	"milonga/internal/app"
 
+	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
 
+// explose GET /health and GET /ping
 func ActivateRoutes(app *app.App, services []CriticalService, dbs2check []*gorm.DB) {
 
 	healtchecker := NewHealthCheck(app, services, dbs2check)
 	app.Server.Get("/health", healtchecker.Check)
+
+	app.Server.Get("ping", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"status": "ok",
+		})
+	})
 
 }
