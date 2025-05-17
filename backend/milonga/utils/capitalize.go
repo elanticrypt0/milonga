50236ed25a508e6c0463640667da6a6decdb3ed0
k23dev <hashersmasher@proton.me>
2025-05-17 18:35:04 -0300

    add: función para capitalizar strings

---
+++ backend/milonga/utils/capitalize.go
@@ -0,0 +1,12 @@
+package utils
+
+import "unicode"
+
+func Capitalize(s string) string {
+	if s == "" {
+		return ""
+	}
+	runes := []rune(s)
+	runes[0] = unicode.ToUpper(runes[0])
+	return string(runes)
+}
